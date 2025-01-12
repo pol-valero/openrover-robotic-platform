11194fe4c02290b7ac5efa40e5dd6543829b31c0
Pol Valero <pol.valero.arjona@gmail.com>
2025-01-12 11:40:22 +0100

    Finished disabled states of main buttons in home screen

--- RoverScreenDesign_events.py
+++ RoverScreenDesign_events.py
@@ -2,3 +2,27 @@
 def clickedButton(event_struct):
     return
 
+
+def headControlSwChecked(event_struct):
+    return
+
+
+def headControlSwUnchecked(event_struct):
+    return
+
+
+def armControlSwChecked(event_struct):
+    return
+
+
+def armControlSwUnchecked(event_struct):
+    return
+
+
+def selfRotationControlSwChecked(event_struct):
+    return
+
+
+def selfRotationControlSwUnchecked(event_struct):
+    return
+
